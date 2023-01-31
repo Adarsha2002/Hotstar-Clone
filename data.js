c8fe55cfe7b7796c94fdc9d723aac5f911402723
Adarsha <adarshaghosh54@gmail.com>
2023-01-31 20:05:04 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,72 @@
+let movies = [
+    {
+      name: "Doctor Strange in the Multiverse of Madness",
+      des:
+        "When the Multiverse is unlocked, Doctor Strange must enlist help from old and new allies in order to confront a surprising adversary.",
+      image: "sliders/slider 9.webp"
+    },
+  
+    {
+      name: "Ezra",
+      des:
+        "Priya has recently moved to Kochi along with her husband and brings along an antique Jewish trunk, unaware that it contains a dybbuk, a revenge-hungry spirit.",
+      image: "sliders/slider 10.png"
+    },
+  
+    {
+      name: "2AM",
+      des:
+        "New to the city, a medical student settles in a hostel. Her life turns upside down when she experiences mysterious happenings, unfolding a horrifying past.",
+      image: "sliders/slider 8.png"
+    },
+  
+    {
+      name: "Angrezi Medium",
+      des:
+        "Champak, a simple Mithai-seller, will go to any lengths to fulfil daughter Tarika's dream of studying in London, triggering a chain of hilarious misadventures.",
+      image: "sliders/slider 7.webp"
+    },
+  
+    {
+      name: "9Hours",
+      des:
+        "Three prisoners escape from Hyderabad Central Jail to form three teams to loot three banks. While two teams execute the heist successfully, what happens to the third team?",
+      image: "sliders/slider 6.png"
+    },
+  
+    {
+      name: "Luca",
+      des:
+        "The movie is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides.",
+      image: "sliders/slider 5.PNG"
+    },
+  
+    {
+      name: "Raya and the Last Dragon",
+      des:
+        "Raya, a fallen princess, must track down the legendary last dragon to stop the evil forces that have returned and threaten her world.",
+      image: "sliders/slider 4.PNG"
+    },
+  
+    {
+      name: "WandaVision",
+      des:
+        "Wanda Maximoff and Vision-two super-powered beings living idealized suburban lives-begin to suspect that everything is not as it seems.",
+      image: "sliders/slider 3.PNG"
+    },
+  
+    {
+      name: "The Falcon and The Winter Soldier",
+      des:
+        "Following the events of 'Avengers: Endgame' Sam Wilson and Bucky Barnes team up in a global adventure that tests their abilities and their patience.",
+      image: "sliders/slider 2.PNG"
+    },
+  
+    {
+      name: "Loki",
+      des:
+        "After stealing the Tesseract in “Avengers: Endgame,” Loki lands before the Time Variance Authority.",
+      image: "sliders/slider 1.PNG"
+    }
+  ];
+  
